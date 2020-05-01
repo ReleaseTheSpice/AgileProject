3c51e5b1ed81a635f79f652751aa5aaa68443e45
ReleaseTheSpice <mvanlaethem0@gmail.com>
2020-04-30 21:04:50 -0700

    Added front-End angular Forum page!

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,90 +1,92 @@
 // @ts-ignore
-import { Component  } from "@angular/core";
+import { Component  } from "@angular/core";
 // @ts-ignore
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ApiService } from './ApiService';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ApiService } from './ApiService';
 @Component({
-  selector: 'app-root',
-  template: `
-<h1> Gold Gamer Experience </h1>
-<nav>
-  <a routerLink="/main" routerLinkActive="active">Main</a> |
-  <a routerLink="/forum" routerLinkActive="active">Forum</a>
-  <span *ngIf="message == 'Not logged in.'"> | <a routerLink="/register" routerLinkActive="active">Register</a> </span>
-  <span *ngIf="message == 'Not logged in.'"> | <a routerLink="/login" routerLinkActive="active">Login</a> </span>
-  <span *ngIf="admin"> | <a routerLink="/eventManager" routerLinkActive="active">Gamer Event Manager</a> </span>
-  <span *ngIf="message == 'The user has been logged in.'"> | <a routerLink="/myEvents" routerLinkActive="active">My Gamer Events</a> </span>
-  <span *ngIf="message == 'The user has been logged in.'"> | <a routerLink="/main" routerLinkActive="active" (click)="logout()">Logout ({{username}})</a> </span>
-</nav>
-<br>
-<!-- Where router should display a view -->
-<router-outlet></router-outlet>`
+  selector: 'app-root',
+  template: `
+    <h1> Gold Gamer Experience </h1>
+    <h3> A destination for gaming commodities and communities. </h3>
+    <nav>
+      <a routerLink="/main" routerLinkActive="active">Main</a> |
+      <a routerLink="/forum" routerLinkActive="active">Forum</a>
+      <span *ngIf="message == 'Not logged in.'"> | <a routerLink="/register" routerLinkActive="active">Register</a> </span>
+      <span *ngIf="message == 'Not logged in.'"> | <a routerLink="/login" routerLinkActive="active">Login</a> </span>
+      <span *ngIf="admin"> | <a routerLink="/eventManager" routerLinkActive="active">Event Manager</a> </span>
+      <span *ngIf="message == 'The user has been logged in.'"> | <a routerLink="/myEvents" routerLinkActive="active">My events</a> </span>
+      <span *ngIf="message == 'The user has been logged in.'"> | <a routerLink="/main" routerLinkActive="active" (click)="logout()">Logout ({{username}})</a> </span>
+    </nav>
+    <br>
+    <!-- Where router should display a view -->
+    <router-outlet></router-outlet>`
 })
-export class AppComponent {
-  // Hard-code credentials for convenience.
-  admin                 = false;
-  username              = '';
-  token                 = '';
-  message               = 'Not logged in.';
-  secureData:string     = '';
+export class AppComponent {
+  // Hard-code credentials for convenience.
+  admin                 = false;
+  username              = '';
+  test  = '';
+  token                 = '';
+  message               = 'Not logged in.';
+  secureData:string     = '';
   reqInfo:any           = null;
   _apiService:ApiService;
-  public site='http://localhost:1337/';
+  public site='http://localhost:1337/';
 
 
-  // Since we are using a provider above we can receive 
-  // an instance through an constructor.
-  constructor(private http: HttpClient) {
-    // Pass in http module and pointer to AppComponent.
-    this._apiService = new ApiService(http, this);
+  // Since we are using a provider above we can receive
+  // an instance through an constructor.
+  constructor(private http: HttpClient) {
+    // Pass in http module and pointer to AppComponent.
+    this._apiService = new ApiService(http, this);
     this.showContentIfLoggedIn();
   }
 
   //------------------------------------------------------------
-  // Either shows content when logged in or clears contents.
+  // Either shows content when logged in or clears contents.
   //------------------------------------------------------------
-  showContentIfLoggedIn() {
-    // Logged in if token exists in browser cache.
-    if(sessionStorage.getItem('auth_token')!=null) {
-      this.token   = sessionStorage.getItem('auth_token');
-      this.message = "The user has been logged in.";
+  showContentIfLoggedIn() {
+    // Logged in if token exists in browser cache.
+    if(sessionStorage.getItem('auth_token')!=null) {
+      this.token   = sessionStorage.getItem('auth_token');
+      this.message = "The user has been logged in.";
       this.getSecureData()
     }
-    else {
-      this.message = "Not logged in.";
-      this.token   = ''
+    else {
+      this.message = "Not logged in.";
+      this.token   = ''
     }
   }
 
-  getSecureData() {
+  getSecureData() {
     this._apiService.getData('User/SecureAreaJwt',
-      this.secureDataCallback);
+        this.secureDataCallback);
   }
-  // Callback needs a pointer '_this' to current instance.
-  secureDataCallback(result, _this) {
-    if(result.errorMessage == "") {
-      _this.secureData = result.secureData;
+  // Callback needs a pointer '_this' to current instance.
+  secureDataCallback(result, _this) {
+    if(result.errorMessage == "") {
+      _this.secureData = result.secureData;
       _this.reqInfo = result.reqInfo;
       _this.username = result.reqInfo.username;
       if (result.reqInfo.roles.indexOf('Admin') >= 0) {
         _this.admin = true;
       }
     }
-    else {
+    else {
       alert(JSON.stringify(result.errorMessage));
     }
   }
 
   //------------------------------------------------------------
-  // Log user out. Destroy token.
+  // Log user out. Destroy token.
   //------------------------------------------------------------
-  logout() {
+  logout() {
     sessionStorage.clear();
     this.showContentIfLoggedIn();
 
-    // Clear data.
-    this.secureData    = "";
-    this.reqInfo       = {};
+    // Clear data.
+    this.secureData    = "";
+    this.reqInfo       = {};
     this.username      = '';
     this.admin         = false;
     // Redirect home and reload that page (sorry for using this easy abd dirty way)
--- angular/src/app/app.forum.ts
+++ angular/src/app/app.forum.ts
@@ -1,20 +1,122 @@
 import { Component }       from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ApiService} from "./ApiService";
 import { Router } from '@angular/router';
 
+
+const BASE_URL = "http://localhost:1337/Message/";
 
 @Component({
     templateUrl: './app.forum.html'
 })
 
-//FIXME
 export class Forum {
-    _apiService: ApiService;
-    events: Array<any>;
-    username: string = '';
+    _messagesArray: Array<any>;
+    _http:HttpClient;
+    _id:Number;
+    _date:Date;
+    _errorMessage:String = "";
+    _message:String = "";
+    _authorName:String = "";
+    _content:String = "";
+    admin                 = false;
+    username              = '';
+    test  = '';
+    token                 = '';
+    message               = 'Not logged in.';
+    secureData:string     = '';
+    reqInfo:any           = null;
+    _apiService:ApiService;
+    public site='http://localhost:1337/';
 
     constructor(private http: HttpClient, private router: Router) {
+        this._http = http;
+        this._apiService = new ApiService(http, this);
+        this.showContentIfLoggedIn();
+        this.getAllMessages();
     }
 
+    showContentIfLoggedIn() {
+        // Logged in if token exists in browser cache.
+        if(sessionStorage.getItem('auth_token')!=null) {
+            this.token   = sessionStorage.getItem('auth_token');
+            this.message = "The user has been logged in.";
+            this.getSecureData()
+        }
+        else {
+            this.message = "Not logged in.";
+            this.token   = ''
+        }
+    }
+
+    getSecureData() {
+        this._apiService.getData('User/SecureAreaJwt',
+            this.secureDataCallback);
+    }
+    // Callback needs a pointer '_this' to current instance.
+    secureDataCallback(result, _this) {
+        if(result.errorMessage == "") {
+            _this.secureData = result.secureData;
+            _this.reqInfo = result.reqInfo;
+            _this.username = result.reqInfo.username;
+            if (result.reqInfo.roles.indexOf('Admin') >= 0) {
+                _this.admin = true;
+            }
+        }
+        else {
+            alert(JSON.stringify(result.errorMessage));
+        }
+    }
+
+    getAllMessages() {
+        let url = BASE_URL + 'GetMessages'
+        this._http.get<any>(url)
+            // Get data and wait for result.
+            .subscribe(result => {
+                    this._messagesArray = result.messages;
+                },
+
+                error =>{
+                    // Let user know about the error.
+                    this._errorMessage = error;
+                })
+    }
+
+    createMessage() {
+        // This free online service receives post submissions.
+        this.http.post(BASE_URL + "CreateMessage",
+            {
+                //_id:            this._id,
+                author:         this.username, // FIX THISSSS
+                content:        this._content,
+                date:           Date.now()
+                //replies:
+                //reply:
+            })
+            .subscribe(
+                // Data is received from the post request.
+                (data) => {
+                    // Inspect the data to know how to parse it.
+                    console.log("POST call successful. Inspect response.",
+                        JSON.stringify(data));
+                    if (data["errorMessage"] == "") {
+                        this._message = "Message added!"
+                        this._errorMessage = ""
+                    }
+                    else {
+                        this._errorMessage = "Failed to add message."
+                        this._message = ""
+                    }
+                    // this._errorMessage = data["errorMessage"];
+                    this.getAllMessages();
+                },
+                // An error occurred. Data is not received.
+                error => {
+                    this._errorMessage = error;
+                });
+    }
+
+    clearMessage() {
+        this._content = "";
+    }
 }
